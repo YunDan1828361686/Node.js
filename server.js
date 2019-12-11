fedb55c87448838cd32bfec0e77afe04d15c14d1
姜彦博 <1828361686@qq.com>
2019-12-11 11:32:51 +0800

    demo

--- server.js
+++ server.js
@@ -7,13 +7,6 @@ app.use(express.json());
 // 定义数据
 const { pagination1, pagination2, pagination3 } = require('./Json/table');
 
-// 定义接口
-app.post("/node/post", function (req, res) {
-    res.json({
-        err: 0,
-        msg: "post请求接口数据"
-    });
-});
 app.post("/node/table", function (req, res) {
     if (req.body.page_current == 1 || req.body.page_current == "") {
         if (req.body.page_size == 20) {
@@ -78,6 +71,99 @@ app.post("/logout", function (req, res) {
         msg: "退出登录！"
     });
 });
+app.post("/node/echarts", function (req, res) {
+    res.json({
+        code: 0,
+        msg: "请求成功!",
+        data: [
+            {
+                title: "BBU1",
+                xAxis: [
+                    "测试1",
+                    "测试2",
+                    "测试3",
+                    "测试4",
+                    "测试5",
+                    "测试6",
+                    "测试7",
+                    "测试8",
+                    "测试9",
+                    "测试10",
+                    "测试11",
+                    "测试12",
+                    "测试13",
+                    "测试14",
+                    "测试15"
+                ],
+                yAxis: [{
+                    name: "电量1",
+                    // type: "line"  写死
+                    type: "line",
+                    color: "#2d8cf0",
+                    data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+                },
+                {
+                    name: "电量2",
+                    // type: "line"  写死
+                    type: "line",
+                    color: "#2d8cf0",
+                    data: [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+                },
+                {
+                    name: "电量3",
+                    // type: "line"  写死
+                    type: "line",
+                    color: "#2d8cf0",
+                    data: [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
+                }],
+                RRUname: ["RRU1", "RRU2", "RRU3"]
+            },
+            {
+                title: "BBU2",
+                xAxis: [
+                    "测试1",
+                    "测试2",
+                    "测试3",
+                    "测试4",
+                    "测试5",
+                    "测试6",
+                    "测试7",
+                    "测试8",
+                    "测试9",
+                    "测试10",
+                    "测试11",
+                    "测试12",
+                    "测试13",
+                    "测试14",
+                    "测试15"
+                ],
+                yAxis: [{
+                    name: "电量1",
+                    // type: "line"  写死
+                    type: "line",
+                    color: "#2d8cf0",
+                    data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+                },
+                {
+                    name: "电量2",
+                    // type: "line"  写死
+                    type: "line",
+                    color: "#2d8cf0",
+                    data: [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+                },
+                {
+                    name: "电量3",
+                    // type: "line"  写死
+                    type: "line",
+                    color: "#2d8cf0",
+                    data: [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
+                }],
+                RRUname: ["RRU1", "RRU2", "RRU3"]
+            }
+        ]
+    });
+});
+
 
 
 app.listen(3000, function () {
