441075daccff5e091b3f223f7ddc7f6db3a79cce
YunDan1828361686 <1828361686@qq.com>
2020-09-28 15:27:57 +0800

    登录接口

--- server.js
+++ server.js
@@ -12,6 +12,26 @@ app.use('/node1', require('./routes/node1'));
 // demo一系列如echarts
 app.use('/node2', require('./routes/node2'));
 
+app.post("/login", function (req, res) {
+    let data = {
+        super_admin: {
+            name: 'super_admin',
+            user_id: '1',
+            access: ['super_admin', 'admin'],
+            token: 'super_admin',
+            avatar: 'https://file.iviewui.com/dist/a0e88e83800f138b94d2414621bd9704.png'
+        },
+        admin: {
+            name: 'admin',
+            user_id: '2',
+            access: ['admin'],
+            token: 'admin',
+            avatar: 'https://avatars0.githubusercontent.com/u/20942571?s=460&v=4'
+        }
+    }
+    res.json(data[req.body.userName]);
+});
+
 app.post("/logout", function (req, res) {
     res.json({
         err: 0,
